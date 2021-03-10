18b2847ffb824f41581fc53af56d1e599e75bc05
Vlad <79570288+vladyslav-what-is-love@users.noreply.github.com>
2021-03-10 20:33:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const abs = require("./abs");
+const summ = require("./summ");
+const min = require("./min");
+
+let numbers = [-5,-4,-2,0,1,5,10,15];
+
+console.log(abs(-5));
+console.log(summ(numbers));
+console.log(min(numbers));
